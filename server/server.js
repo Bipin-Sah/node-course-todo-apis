85a394e427d98ab7de501b7cb557846e7038f3fe
Bipin-Sah <bipinkumar.sat@gmail.com>
2018-08-25 21:59:40 +0530

    Init commit

---
+++ server/server.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+var Todo = mongoose.model('Todo', {
+  text:{
+    type : String
+  },
+  completed: {
+    type: Boolean
+  },
+  completedAt:{
+    type: Number
+  }
+});
+
+var newTodo = new Todo({
+  text:'Cook dinner',
+  completed: false
+});
+
+newTodo.save().then((doc) =>{
+  console.log(JSON.stringfy(doc, undefined,2));
+}, (e) =>{
+  console.log('Unable to save todo')
+});
