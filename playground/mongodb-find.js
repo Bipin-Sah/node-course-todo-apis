2698fa98f4a46723cc54e204680c302fc8e70b24
Bipin-Sah <41326298+Bipin-Sah@users.noreply.github.com>
2018-08-27 14:19:41 +0530

    Update mongodb-find.js

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -1,6 +1,8 @@
 const {MongoClient,ObjectID }= require('mongodb');
+//DB Name : TodoApp / Users
+const url = 'mongodb://localhost:27017/Users';
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client)=>{
+MongoClient.connect(url, (err, client)=>{
 
   if(err)
   {
@@ -8,15 +10,27 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client)=>{
 }
 console.log('Connected to MongoDB server');
 
-const db= client.db('TodoApp');
+const db = client.db('Users');
 
-db.collection('Todos').aggregate([
-    {$match : {completed: false}},
-    $TotalAge
-    {$sum : '$age'}
-  ])
+// db.collection('user').aggregate([
+//   {$match: {"_id": new ObjectID("5b8399672e9df10af0327bec")}},
+//   {$group: {
+//     _id: null,
+//     TotalAge:  {$sum: "$Age"}
+//   }}
+// ]).toArray().then((docs) =>{
+//   console.log('Users');
+//   console.log(JSON.stringify(docs));
+// },(err) => {
+//   console.log("Unable to fetch Users: " +err);
+// });
 
+db.collection('user').find({"name": "Bipin1"}).toArray().then((docs) => {
+  console.log('Users');
+  console.log(JSON.stringify(docs));
+}, (err) => {
+  console.log('Unable to fetch todos', err);
+});
 
-);
-//client.close();
+client.close();
 });
