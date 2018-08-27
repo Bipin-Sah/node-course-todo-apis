6b9bb5340f8ceeec701e656fb716c65e1ccc122a
Bipin-Sah <41326298+Bipin-Sah@users.noreply.github.com>
2018-08-27 14:17:58 +0530

    Update mongodb-connect.js

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -4,12 +4,12 @@ const {MongoClient, ObjectID} = require('mongodb');
 var obj = new ObjectID();
 console.log(obj);
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, client) =>{
+MongoClient.connect('mongodb://localhost:27017/Users',(err, client) =>{
   if(err){
     return console.log('Unable to connect to MongoDB server');
   }
   console.log('Connected to MongoDB server');
-  const db = client.db('TodoApp')
+  const db = client.db('Users')
 
   // db.collection('Todos').insertOne({
   //   text: 'Something to do',
@@ -20,15 +20,15 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, client) =>{
   //   }
   //   console.log(JSON.stringify(result.ops, undefined, 2));
   // });
-  // db.collection('User').insertOne({
-  //   name : 'Bipin kUmar',
-  //   Age: 30,
-  //   Location: 'BTM layout'
-  // },(err, result)=>{
-  //   if(err){
-  //     return console.log('Unable to insert user', err);
-  //   }
-  //   console.log(JSON.stringify(result.ops, undefined));
-  // });
+  db.collection('user').insertOne({
+    name : 'Bipin1',
+    Age: 31,
+    Location: 'BTM layout stage1'
+  },(err, result)=>{
+    if(err){
+      return console.log('Unable to insert user', err);
+    }
+    console.log(JSON.stringify(result.ops, undefined));
+  });
   client.close();
 });
