0c1520bf41e67a4a3461793383cd8eeba5fbdc1e
Bipin-Sah <bipinkumar.sat@gmail.com>
2018-08-28 14:33:29 +0530

    Add Server changes

--- server/server.js
+++ server/server.js
@@ -1,27 +1,39 @@
 var mongoose = require('mongoose');
-
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-var Todo = mongoose.model('Todo', {
-  text:{
-    type : String
+const url = 'mongodb://localhost:27017/TodoApp';
+mongoose.connect(url);
+
+var Todo = mongoose.model('Todo',{
+  text: {
+    type: String
   },
-  completed: {
+  completed:{
     type: Boolean
   },
-  completedAt:{
+  completedAt: {
     type: Number
   }
 });
 
-var newTodo = new Todo({
-  text:'Cook dinner',
-  completed: false
+// var newTodo = new Todo({
+//   text: 'Cook dinner',
+// });
+//
+// newTodo.save().then((doc)=> {
+//   console.log('Saved todo', doc);
+// }, (err)=>{
+//   console.log('unable to save todo');
+// });
+
+var otherTodo = new Todo({
+  text: 'feed the cat',
+  completed: true,
+  completedAt: 345
 });
 
-newTodo.save().then((doc) =>{
-  console.log(JSON.stringfy(doc, undefined,2));
-}, (e) =>{
-  console.log('Unable to save todo')
+otherTodo.save().then((doc)=>{
+  console.log(JSON.stringify(doc, undefined, 2));
+}, (e)=>{
+  console.log('unbale to save OtherTodo');
 });
